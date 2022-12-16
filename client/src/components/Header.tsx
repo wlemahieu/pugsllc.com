159322a193cda0cc8554eb68d6fecd8ede8daf15
Wesley LeMahieu <lateralis3k@gmail.com>
2022-12-16 10:41:51 -0800

    Add favicon and logo to header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,10 +1,12 @@
 import { Component } from 'solid-js';
 import styles from '@components/Header.module.css';
 import Typography from '@suid/material/Typography';
+import logo from '@assets/logo.jpg';
 
 const Header: Component = () => {
   return (
     <header class={styles.root}>
+      <img class={styles.logo} src={logo} />
       <Typography variant="h3" gutterBottom>
         pugsllc.com
       </Typography>
