9ef68c03711d5949eb3977a82bd1267dae602445
Wesley LeMahieu <lateralis3k@gmail.com>
2022-12-17 14:52:38 -0800

    Fix page flexbox alignment for dissimilar size views

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,8 +1,7 @@
 /**
  * App structure, session fetch, socket listeners
  */
-import { Component, createContext, onMount, Show } from 'solid-js';
-import { createStore, SetStoreFunction, Store } from 'solid-js/store';
+import { Component, Show } from 'solid-js';
 import { Router } from '@solidjs/router';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
@@ -11,31 +10,7 @@ import CssBaseline from '@suid/material/CssBaseline';
 import Container from '@suid/material/Container';
 import { createTheme, ThemeProvider } from '@suid/material/styles';
 import CookiesPopup from './CookiesPopup';
-
-const functionURL = 'https://pugsllc-com-b3us3jciya-uw.a.run.app';
-
-const v1GetMyUser = async (): Promise<string> => {
-  const url = import.meta.env.PROD ? functionURL : 'http://localhost:3333';
-  const path = '/v1/me';
-  const results = await fetch(`${url}${path}`, {
-    method: 'GET',
-    credentials: 'include',
-  });
-  return await results.text();
-};
-
-interface DefaultValuesI {
-  sessionID: string;
-}
-
-type StoreT = [Store<DefaultValuesI>, SetStoreFunction<DefaultValuesI>];
-
-const defaultValues: DefaultValuesI = {
-  sessionID: '',
-};
-
-export type AppContextT = DefaultValuesI;
-export const AppContext = createContext<AppContextT>();
+import styles from './App.module.css';
 
 const theme = createTheme({
   typography: {
@@ -68,31 +43,19 @@ const theme = createTheme({
 });
 
 const App: Component = () => {
-  const [state, setState]: StoreT = createStore(defaultValues);
-
-  onMount(async () => {
-    const sessionID = await v1GetMyUser();
-    setState('sessionID', () => sessionID);
-  });
-
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <AppContext.Provider value={state}>
-        <Show when={true}>
-          <Router>
-            <Container
-              maxWidth="md"
-              sx={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}
-            >
-              <Header />
-              <Routes />
-              <Footer />
-              <CookiesPopup />
-            </Container>
-          </Router>
-        </Show>
-      </AppContext.Provider>
+      <Show when={true}>
+        <Router>
+          <Container maxWidth="md" class={styles.root}>
+            <Header />
+            <Routes />
+            <Footer />
+            <CookiesPopup />
+          </Container>
+        </Router>
+      </Show>
     </ThemeProvider>
   );
 };
